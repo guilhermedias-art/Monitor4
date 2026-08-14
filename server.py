5b3b94f52db04e9f13f7da0cd52ed909fea4dd4e
Guilherme Dias Cavalheri <gui.dias.c12@gmail.com>
2026-08-14 10:00:57 -0300

    Estrutura inicial de servidor com tentativa de criação de servidor com conexão ao cliente, com informações de CPU E RAM
    Updated server configuration to use port 5000 and changed variable names to Portuguese.

--- server.py
+++ server.py
@@ -3,27 +3,36 @@ import sys
 import psutil
 import time
 
-HOST = "127.0.0.1"
-PORT = 1080
+HOST = '127.0.0.1'
+PORT = 5000
 NUM_BYTES = 1024
 
 tempo_atual = time.time()
  
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
+server.bind((HOST,PORT))
 
 server.listen(1)
 
-connection, address = server.accept()
-
-def input_dados(connection) :
+conexao, endereço = server.accept()
+with conexao:
+    print('Conectado no :', endereço)
     while True:
-        dados = connection.recv(NUM_BYTES)
+         dados = conexao.recv(NUM_BYTES)
+         if not dados:
+            conexao.sendall(dados)
+
+        
+
+
+def input_dados(conexao) :
+    while True:
         if not dados:
             print("Cliente não conseguiu se conectar")
             break
         msg = dados.decode('utf-8')
         print('{tempo_atual} ' > CONECTADO)
+        print('Serviço conectado em {HOST}{PORT}')
 
 
 
@@ -32,6 +41,7 @@ def input_dados(connection) :
 def enviar_dados() :
     cpu = psutil.cpu_percent()
     memoria = psutil.virtual_memory().percent 
-    
+    print('Uso de CPU em % = {cpu}')
+    print('Uso de RAM em % = {memoria}')
 
 

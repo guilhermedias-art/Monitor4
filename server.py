473f292cb36b0f79e8b8aba2eab66209395bd7e9
Guilherme Dias Cavalheri <gui.dias.c12@gmail.com>
2026-08-17 10:07:03 -0300

    Servidor criado e conectando com o cliente
    Refactor server to use new port and improve message handling.

--- server.py
+++ server.py
@@ -4,44 +4,53 @@ import psutil
 import time
 
 HOST = '127.0.0.1'
-PORT = 5000
+PORT = 4998
 NUM_BYTES = 1024
 
-tempo_atual = time.time()
- 
+tempo_formatado = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+def decodificar_mensagem(conn):
+    dados = conn.recv(NUM_BYTES)
+    if not dados:
+        print("Cliente não conseguiu se conectar")
+
+    mensagem_decodificada = dados.decode("utf-8")
+
+    if mensagem_decodificada == 'q':
+        quit()
+
+    print(mensagem_decodificada)
+    
+
+def enviar_dados(conn):
+
+    cpu = psutil.cpu_percent()
+    memoria = psutil.virtual_memory().percent 
+    print(f'Uso de CPU em % = {cpu}')
+    print(f'Uso de RAM em % = {memoria}')
+
+
+
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST,PORT))
 
 server.listen(1)
-
-conexao, endereço = server.accept()
-with conexao:
-    print('Conectado no :', endereço)
-    while True:
-         dados = conexao.recv(NUM_BYTES)
-         if not dados:
-            conexao.sendall(dados)
-
-        
-
-
-def input_dados(conexao) :
-    while True:
-        if not dados:
-            print("Cliente não conseguiu se conectar")
-            break
-        msg = dados.decode('utf-8')
-        print('{tempo_atual} ' > CONECTADO)
-        print('Serviço conectado em {HOST}{PORT}')
+while True:
+    conexao, endereço = server.accept()
+    print('Serviço conectado no :', endereço)
+    decodificar_mensagem(conexao)
+    print(f"{tempo_formatado} -> CONECTADO")
+    enviar_dados(conexao)
 
 
 
 
 
-def enviar_dados() :
-    cpu = psutil.cpu_percent()
-    memoria = psutil.virtual_memory().percent 
-    print('Uso de CPU em % = {cpu}')
-    print('Uso de RAM em % = {memoria}')
+
+
+
+
 
 

5b8ff3de5c715199134d03c56b4025efddfabd0c
Guilherme Dias Cavalheri <gui.dias.c12@gmail.com>
2026-08-19 09:52:01 -0300

    Implementação de threads em cliente e servidor,  para comunicação simples
    Refactored server to use threads for handling client connections and data sending.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import psutil
 import time
+from threading import Thread
 
 HOST = '127.0.0.1'
 PORT = 4998
@@ -10,24 +11,32 @@ NUM_BYTES = 1024
 tempo_formatado = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 def decodificar_mensagem(conn):
-    dados = conn.recv(NUM_BYTES)
-    if not dados:
-        print("Cliente não conseguiu se conectar")
+    try:
+        while True:
+            dados = conn.recv(NUM_BYTES)
+            if not dados:
+                print("Cliente não conseguiu se conectar")
+                break
+            print(f"{tempo_formatado} -> CONECTADO")
+            mensagem_decodificada = dados.decode("utf-8")
+            if mensagem_decodificada == 'q':
+                print('Voce encerrou a conexão com o servidor')
+                break
 
-    mensagem_decodificada = dados.decode("utf-8")
 
-    if mensagem_decodificada == 'q':
-        quit()
+            print(mensagem_decodificada)
+    except Exception as e:
+            print("Erro no armazenamento de dados")
 
-    print(mensagem_decodificada)
     
 
 def enviar_dados(conn):
-
-    cpu = psutil.cpu_percent()
-    memoria = psutil.virtual_memory().percent 
-    print(f'Uso de CPU em % = {cpu}')
-    print(f'Uso de RAM em % = {memoria}')
+    while True:
+            cpu = psutil.cpu_percent()
+            memoria = psutil.virtual_memory().percent 
+            mensagem = (f'Uso de CPU em % = {cpu} , Uso de RAM em % = {memoria}')
+            conn.sendall(mensagem.encode('utf-8'))
+            time.sleep(5)
 
 
 
@@ -39,18 +48,15 @@ server.bind((HOST,PORT))
 server.listen(1)
 while True:
     conexao, endereço = server.accept()
-    print('Serviço conectado no :', endereço)
-    decodificar_mensagem(conexao)
-    print(f"{tempo_formatado} -> CONECTADO")
-    enviar_dados(conexao)
+    print('Serviço conectado no :', endereço)         
+    thread1 = Thread(target=decodificar_mensagem, args=(conexao,),daemon=True)
+    thread2 = Thread(target=enviar_dados, args=(conexao,),daemon = True)
 
 
 
 
+    thread1.start()
+    thread2.start()
 
-
-
-
-
-
-
+    thread1.join()
+    thread2.join()

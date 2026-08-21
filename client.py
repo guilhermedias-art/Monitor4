d313c555862ea195c9e863b3e29fc0ab8f8efe4e
Tpotsk <tpotsk@gmail.com>
2026-08-21 19:04:13 +0000

    feat: listar monitores + mensagens

--- client.py
+++ client.py
@@ -9,12 +9,6 @@ cliente = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 cliente.connect((HOST,PORT))
 
 def envio_d_dados(client) :
-    print("MENU:\n" \
-    "Monitorar CPU -> CPU<(tempo)\n" \
-    "Monitorar Memoria -> MEM<(tempo)\n" \
-    "Sair -> quit\n" \
-    "Terminar -> exit\n")
-
     while True:
         mensagem = input("")
         client.sendall(mensagem.encode('utf-8'))
@@ -23,7 +17,7 @@ def envio_d_dados(client) :
 def exibir_msg(client):
         while True:
             dados_p_decodificar = client.recv(NUM_BYTES)
-            if not dados_p_decodificar :
+            if not dados_p_decodificar:
                 print("A mensagem que o usuário digitou não conseguiu ser lida")
                 quit()
             mensagem_decodificada = dados_p_decodificar.decode('utf-8')
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import sys
 import psutil
 import time
-from threading import Thread
+from threading import Thread, Event
 import queue
 
 HOST = '127.0.0.1'
@@ -12,10 +12,12 @@ NUM_BYTES = 1024
 tempo_formatado = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 fila_msg = queue.Queue()
+threads_monitores = {}
 
 def decodificar_mensagem(conn):
     try:
-        msg = f"{tempo_formatado} -> CONECTADO"
+        msg = f"{tempo_formatado} : CONECTADO!"
+        number = 0
         fila_msg.put(msg)
 
         while True:
@@ -27,32 +29,50 @@ def decodificar_mensagem(conn):
 
             mensagem_decodificada = dados.decode("utf-8")
 
-            if(mensagem_decodificada.upper() == 'QUIT'):
-                msg = f"Comando requisitado: Quit"
+            if(mensagem_decodificada.upper() == 'LIST'):
+                listar_monitores()
+                continue
+            
+            elif(mensagem_decodificada.upper() == 'QUIT'):
+                # Desligar um monitor em especifico
+                
+                '''msg = f"Comando requisitado: Quit"
                 fila_msg.put(msg)
+                break
+                '''
                 continue
 
             elif(mensagem_decodificada.upper() == 'EXIT'):
                 msg = f'Voce encerrou a conexão com o servidor'
                 fila_msg.put(msg)
+                msg = "EXIT"
+                fila_msg.put(msg)
                 break
 
-            if "<" not in mensagem_decodificada:
+            elif "<" not in mensagem_decodificada:
                 msg = f"Digite uma mensagem válida!"
                 fila_msg.put(msg)
                 continue
 
             palavra, arg = mensagem_decodificada.split("<", 1)
+
             if(palavra.upper() == 'CPU'):
                 msg = f"Comando requisitado: CPU {arg}"
                 fila_msg.put(msg)
-                thread_mem = Thread(target=monitoramento, args=(palavra, arg))
-                thread_mem.start()
+                nome = f"Monitor {number}"
+                number = number + 1
+                evento_parar = Event()
+                thread_cpu = Thread(target=monitoramento, args=(nome, evento_parar, palavra, arg))
+                threads_monitores[nome] = {"thread": thread_cpu, "evento": evento_parar}
+                thread_cpu.start()
 
             elif(palavra.upper() == 'MEM'):
                 msg = f"Comando requisitado: MEM {arg}"
                 fila_msg.put(msg)
-                thread_mem = Thread(target=monitoramento, args=(palavra, arg))
+                nome = f"Monitor {number}"
+                number = number + 1
+                evento_parar = Event()
+                thread_mem = Thread(target=monitoramento, args=(nome, evento_parar, palavra, arg))
                 thread_mem.start()
 
             else:
@@ -61,28 +81,47 @@ def decodificar_mensagem(conn):
 
     except Exception as e:
             msg = f"Erro no armazenamento de dados: {e}"
+            fila_msg.put(msg)
 
+def listar_monitores():
+    msg = "\n--- Threads Ativas Atualmente ---\n"
+    fila_msg.put(msg)
 
-def monitoramento(palavra, arg):
+    lista_nomes = list(threads_monitores.keys())
+
+    for indice, nome in enumerate(lista_nomes, start=1):
+        msg = f"[{indice}] {nome}\n"
+        fila_msg.put(msg)
+    
+
+def monitoramento(nome, parada, palavra, arg):
     palavra = palavra.upper()
 
-    while True:
+    while not parada.is_set():
 
         if (palavra == "CPU"):
             cpu = psutil.cpu_percent(interval=1)
-            mensagem = (f'Uso de CPU em % = {cpu}')
+            mensagem = (f'{nome} (CPU) em % = {cpu}')
 
         elif (palavra == "MEM"):
             memoria = psutil.virtual_memory().percent
-            mensagem = (f'Uso de RAM em % = {memoria}')
+            mensagem = (f'{nome}: (RAM) em % = {memoria}')
 
         fila_msg.put(mensagem)
         print(mensagem)
 
         time.sleep(int(arg))
+
+
 def enviar_dados(conn, ):
-    while True:
+    finish = False
+    while not finish:
             msg = fila_msg.get()
+            if(msg.upper() == "EXIT"):
+                finish = True
+                print("Envio de dados encerrado")
+                break
+
             conn.sendall(msg.encode('utf-8'))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -95,11 +134,21 @@ server.listen(1)
 while True:
     conexao, endereço = server.accept()
     print('Serviço conectado no :', endereço)
+
+    msg = "Menu de Comandos:\n" \
+            "Listar Monitores = LIST\n" \
+            "Monitorar CPU = CPU-(tempo)\n" \
+            "Monitorar Memoria = MEM-(tempo)\n" \
+            "Sair = quit\n" \
+            "Terminar = exit\n"
+
     thread1 = Thread(target=decodificar_mensagem, args=(conexao,),daemon=True)
     thread2 = Thread(target=enviar_dados, args=(conexao, ),daemon = True)
 
     thread1.start()
     thread2.start()
+
+    fila_msg.put(msg)
 
     thread1.join()
     thread2.join()

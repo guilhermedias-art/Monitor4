4eeaebc2af3195bb6d11776002d4cf861bd6ddc7
Gabriel Cattuzo <gabrielcattuzo@gmail.com>
2026-08-21 08:01:58 -0300

    desenvolvimento thread1

--- server.py
+++ server.py
@@ -14,43 +14,46 @@ tempo_formatado = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 fila_msg = queue.Queue()
 
 def decodificar_mensagem(conn):
-    # Implementar aqui a escolha do comando
     try:
         msg = f"{tempo_formatado} -> CONECTADO"
         fila_msg.put(msg)
 
         while True:
             dados = conn.recv(NUM_BYTES)
-
             if not dados:
                 msg= "Cliente não conseguiu se conectar"
-                fila_msg.append(msg)
+                fila_msg.put(msg)
                 break
 
             mensagem_decodificada = dados.decode("utf-8")
 
-            palavra, arg = mensagem_decodificada.split("<")
+            if(mensagem_decodificada.upper() == 'QUIT'):
+                msg = f"Comando requisitado: Quit"
+                fila_msg.put(msg)
+                continue
 
+            elif(mensagem_decodificada.upper() == 'EXIT'):
+                msg = f'Voce encerrou a conexão com o servidor'
+                fila_msg.put(msg)
+                break
+
+            if "<" not in mensagem_decodificada:
+                msg = f"Digite uma mensagem válida!"
+                fila_msg.put(msg)
+                continue
+
+            palavra, arg = mensagem_decodificada.split("<", 1)
             if(palavra.upper() == 'CPU'):
                 msg = f"Comando requisitado: CPU {arg}"
                 fila_msg.put(msg)
                 thread_mem = Thread(target=monitoramento, args=(palavra, arg))
                 thread_mem.start()
-                
+
             elif(palavra.upper() == 'MEM'):
                 msg = f"Comando requisitado: MEM {arg}"
                 fila_msg.put(msg)
                 thread_mem = Thread(target=monitoramento, args=(palavra, arg))
                 thread_mem.start()
-
-            elif(palavra.upper() == 'QUIT'):
-                msg = f"Comando requisitado: Quit {arg}"
-                fila_msg.put(msg)
-
-            elif(palavra.upper() == 'EXIT'):
-                msg = f'Voce encerrou a conexão com o servidor'
-                fila_msg.put(msg)
-                break
 
             else:
                 msg = f"Digite uma mensagem válida!"
@@ -69,12 +72,11 @@ def monitoramento(palavra, arg):
     elif (palavra == "MEM"):
         memoria = psutil.virtual_memory().percent
         mensagem = (f'Uso de RAM em % = {memoria}')
-    
+
     while True:
         fila_msg.put(mensagem)
         print(mensagem)
         time.sleep(int(arg))
-
 def enviar_dados(conn, ):
     while True:
             msg = fila_msg.get()
@@ -90,7 +92,6 @@ server.listen(1)
 while True:
     conexao, endereço = server.accept()
     print('Serviço conectado no :', endereço)
-
     thread1 = Thread(target=decodificar_mensagem, args=(conexao,),daemon=True)
     thread2 = Thread(target=enviar_dados, args=(conexao, ),daemon = True)
 
